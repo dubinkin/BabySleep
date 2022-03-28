2add5ce50bbbe8c0653b93c2150c68f8dad5fd14
dubinkin <oleg.dubinkin@gmail.com>
2022-03-28 17:56:36 -0500

    Code formatting improvements. Added functions docstrings.

--- src/data_processing/load_data.py
+++ src/data_processing/load_data.py
@@ -6,19 +6,24 @@ import time
 
 def get_sleep(birthday, DATA_PATH):
     '''
-    This function loads raw .csv data acquired from the 'BabyTracker' app and returns
-    a binary time series describing a sleep pattern where '1' corresponds to 'sleep' state and '0' - to 'awake'.
+    This function loads raw .csv data acquired from the 'BabyTracker' app and
+    returns a binary time series describing a sleep pattern where '1'
+    corresponds to 'sleep' state and '0' - to 'awake'.
 
-    The output contains the sleep pattern over the last two months where the timestamps are shifted
-    such that the infants' birthday is effectively set to 01/01/2000 at 0:00 am.
-    The function also returns a timestamp starting from which we will make a prediction.
+    The output contains the sleep pattern over the last two months where the
+    timestamps are shifted such that the infants' birthday is effectively set
+    to 01/01/2000 at 0:00 am. The function also returns a timestamp starting
+    from which we will make a prediction.
 
-    :param birthday: baby's birthday which is needed to align this specific timeseries with the average trend
+    :param birthday: baby's birthday which is needed to align this specific
+                     timeseries with the average trend
     :param DATA_PATH: path to the data directory
-    :return: dataframe containing the last 60 days of sleep data and the timestamp of the latest datapoint
+    :return: dataframe containing the last 60 days of sleep data and the
+             timestamp of the latest datapoint
     '''
 
-    data = pd.read_csv(os.path.join(DATA_PATH, 'raw/csv/Alisa_sleep.csv'), parse_dates=['Time'], index_col='Time')
+    data = pd.read_csv(os.path.join(DATA_PATH, 'raw/csv/Alisa_sleep.csv'),
+                       parse_dates=['Time'], index_col='Time')
     data.columns = ['baby', 'duration', 'note']
     data = data.drop(['baby', 'note'], axis=1)
     currsleep = 0
@@ -26,12 +31,15 @@ def get_sleep(birthday, DATA_PATH):
         currsleep = 1
         last_nap_start = data.index[0]
 
-    #remove naps shorter than 15 minutes as they are likely just noise/mistakes
+    # remove naps shorter than 15 minutes as they are
+    # likely just noise/mistakes
     data = data[data.duration >= 15]
 
     # get the time after which we would like to make a prediction
-    modtimesec = os.path.getmtime(os.path.join(DATA_PATH, 'raw/csv/Alisa_sleep.csv'))
-    finaltime = pd.to_datetime(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modtimesec)))
+    modtimesec = os.path.getmtime(os.path.join(DATA_PATH,
+                                               'raw/csv/Alisa_sleep.csv'))
+    finaltime = pd.to_datetime(time.strftime('%Y-%m-%d %H:%M:%S',
+                                             time.localtime(modtimesec)))
 
     # calculate the number of minutes between the
     #finaldelta = (finaltime - data.index[0]) / np.timedelta64(1, 'm')
@@ -50,7 +58,8 @@ def get_sleep(birthday, DATA_PATH):
     data['minutes'] = (data.index - time_start) / np.timedelta64(1, 'm')
     data.minutes = data.minutes.apply(int)
 
-    finalminutes = int((finaltime - subdate - time_start) / np.timedelta64(1, 'm'))
+    finalminutes = int((finaltime - subdate - time_start)
+                       / np.timedelta64(1, 'm'))
 
     # generate a binary timeseries of sleep/awake status with 10-minute steps
     div = 10
@@ -63,10 +72,13 @@ def get_sleep(birthday, DATA_PATH):
     datalen = data.shape[0]
 
     for i in range(maxt):
-        if data.minutes[interval] <= div * i <= data.minutes[interval] + data.duration[interval]:
+        if (data.minutes[interval] <= div * i) and \
+                (div * i <= data.minutes[interval] + data.duration[interval]):
             data_bin[i] = 1
         elif div * i > data.minutes[interval] + data.duration[interval]:
-            while (interval < datalen - 1) and (div * i > data.minutes[interval] + data.duration[interval]):
+            while (interval < datalen - 1) and \
+                    (div * i > data.minutes[interval]
+                     + data.duration[interval]):
                 interval += 1
 
     # append last sleep interval
@@ -79,11 +91,14 @@ def get_sleep(birthday, DATA_PATH):
     dsf = pd.DataFrame(data_bin, dtype=int)
     dsf.columns = ['sleep']
     dsf = dsf[-60 * 144:]
-    dsf.index = pd.date_range(end=(finaltime - subdate).round(freq='10min'), periods=60 * 144, freq='10min')
+    dsf.index = pd.date_range(end=(finaltime - subdate).round(freq='10min'),
+                              periods=60 * 144, freq='10min')
 
-    #load NYU processed data to add additional predictive features needed for our models
-    trend = pd.read_csv(os.path.join(DATA_PATH, 'NYU_data_processed/NYU_trend.csv')
-                        , parse_dates=['timestamps'], index_col= 'timestamps')
+    # load NYU processed data to add additional
+    # predictive features needed for our models
+    trend = pd.read_csv(os.path.join(DATA_PATH,
+                                     'NYU_data_processed/NYU_trend.csv'),
+                        parse_dates=['timestamps'], index_col= 'timestamps')
 
     # additional features
     dsf['trend'] = trend.trend
--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -17,17 +17,21 @@ if not bot_token:
 
 bot = Bot(token=bot_token)
 
-# Bot dispather and logging
+# Bot dispatcher + loging
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.reply('Hello! I can tell you how sleepy your baby is based on the sleeping '
-                        'data you collected via BabyTracker app. I can also learn your baby\'s '
-                        'sleep patterns and suggest a natural sleeping schedule over the next 24 hours for them.')
-    await bot.send_message(chat_id=message.from_user.id, text= 'Please forward me CSV data from the app to get my estimate.')
+    await message.reply('Hello! I can tell you how sleepy your baby is based '
+                        'on the sleeping data you collected via BabyTracker '
+                        'app. I can also learn your baby\'s sleep patterns '
+                        'and suggest a natural sleeping schedule over the '
+                        'next 24 hours for them.')
+    await bot.send_message(chat_id=message.from_user.id,
+                           text= 'Please forward me CSV data from '
+                                 'the app to get my estimate.')
 
 
 @dp.message_handler(content_types=[types.ContentType.DOCUMENT])
@@ -50,20 +54,24 @@ async def download_doc(message: types.Message):
         score = 2
     sleepdict = ['Not sleepy at all!', 'Somewhat sleepy.', 'Very sleepy!']
 
-    await bot.send_message(chat_id = message.from_user.id, text = 'Sleepy score: {p:1d} out of 3'.format(p = score+1),
-                           parse_mode='Markdown')
-    #await bot.send_message(chat_id=message.from_user.id, text='Raw score: {p:.2f} out of 10'.format(p = rawscore),
-      #                     parse_mode='Markdown')
-    await bot.send_message(chat_id = message.from_user.id, text = sleepdict[score])
+    await bot.send_message(chat_id=message.from_user.id,
+        text='Sleepy score: {p:1d} out of 3'.format(p = score + 1),
+        parse_mode='Markdown')
+
+    await bot.send_message(chat_id=message.from_user.id,
+                           text=sleepdict[score])
 
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ["Yes", "No"]
     keyboard.add(*buttons)
-    await message.answer('Would you like me to generate a suggested sleeping schedule for the next 24 hours?', reply_markup=keyboard)
+    await message.answer('Would you like me to generate a suggested '
+                         'sleeping schedule for the next 24 hours?',
+                         reply_markup=keyboard)
 
 @dp.message_handler(Text(equals='Yes'))
 async def yes_generate(message: types.Message):
-    await message.reply('Give me about 5 to 10 minutes...', reply_markup=types.ReplyKeyboardRemove())
+    await message.reply('Give me about 5 to 10 minutes...',
+                        reply_markup=types.ReplyKeyboardRemove())
 
     # process the data
     sleep, time0 = load_data.get_sleep('12/06/2021', '../data/')
@@ -78,17 +86,24 @@ async def yes_generate(message: types.Message):
     DATA_PATH = '../data/'
     MODEL_PATH = '../src/models/'
 
-    # compute the
-    ans, pred1, pred2, pred3 = ensemble.forecast(sleep, time0, DATA_PATH, MODEL_PATH, forecast_period, weights, conv,
-                                                 conv)
+    # compute the sleep prediction
+    ans, pred1, pred2, pred3 = ensemble.forecast(sleep, time0, DATA_PATH,
+                                                 MODEL_PATH, forecast_period,
+                                                 weights, conv, conv)
 
-    # generate plots of the main forecast, as well as the plots for individual models' predictions
-    predprocess.plot_predict(ans, '../predictions/', 'forecast.png')
-    predprocess.plot_predict(pred1, '../predictions/', 'prediction_LGBM_1step.png')
-    predprocess.plot_predict(pred2, '../predictions/', 'prediction_LGBM_avg.png')
-    predprocess.plot_predict(pred3, '../predictions/', 'prediction_SARIMAX.png')
+    # generate plots of the main forecast, as well
+    # as the plots for individual models' predictions
+    predprocess.plot_predict(ans, '../predictions/',
+                             'forecast.png')
+    predprocess.plot_predict(pred1, '../predictions/',
+                             'prediction_LGBM_1step.png')
+    predprocess.plot_predict(pred2, '../predictions/',
+                             'prediction_LGBM_avg.png')
+    predprocess.plot_predict(pred3, '../predictions/',
+                             'prediction_SARIMAX.png')
 
-    await bot.send_photo(chat_id = message.from_user.id, photo=open('../predictions/forecast.png', 'rb'))
+    await bot.send_photo(chat_id = message.from_user.id,
+                         photo=open('../predictions/forecast.png', 'rb'))
 
     # update the model if needed
     SARIMAXfc.model_update(sleep, time0, MODEL_PATH)
@@ -96,13 +111,27 @@ async def yes_generate(message: types.Message):
 
 @dp.message_handler(Text(equals='No'))
 async def dont_generate(message: types.Message):
-    await message.reply("All right! See you soon!", reply_markup=types.ReplyKeyboardRemove())
+    await message.reply("All right! See you soon!",
+                        reply_markup=types.ReplyKeyboardRemove())
     # process the data
     sleep, time0 = load_data.get_sleep('12/06/2021', '../data/')
     MODEL_PATH = '../src/models/'
 
     # update the model if needed
     SARIMAXfc.model_update(sleep, time0, MODEL_PATH)
+
+@dp.message_handler(commands='update')
+async def cmd_start(message: types.Message):
+    await message.reply('Begin updating the SARIMAX '
+                        'model with the most recent data')
+
+    # update the model
+    sleep, time0 = load_data.get_sleep('12/06/2021', '../data/')
+    MODEL_PATH = '../src/models/'
+    SARIMAXfc.model_update(sleep, time0, MODEL_PATH, force_update=True)
+
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='The model updated successfully!')
 
 
 if __name__ == "__main__":
